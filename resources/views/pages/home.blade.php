c4cdba21241040755efa686b28770cf733411e5e
Mark van Eijk <mark@vaneijk.co>
2020-01-23 23:03:22 +0100

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,5 +2,5 @@
 
 @section('main')
     <h2>Hello</h2>
-    <p>I'm a <a href="https://github.com/markvaneijk" rel="noopener">Web Developer</a>, <a href="https://vormkracht10.nl">entrepeneur</a>, <a href="{{ route('posts') }}">tinkerer</a> and <a href="{{ route('now') }}">currently working</a> on <a href="https://rocketee.rs">Rocketeers</a>.</p>
+    <p>I'm a <a href="https://github.com/markvaneijk" rel="noopener">Web Developer</a>, <a href="https://vormkracht10.nl">entrepeneur</a>, <a href="https://vaneijk.co">company of one</a>, <a href="{{ route('posts') }}">tinkerer</a> and <a href="{{ route('now') }}">currently working</a> on <a href="https://rocketee.rs">Rocketeers</a>.</p>
 @endsection
