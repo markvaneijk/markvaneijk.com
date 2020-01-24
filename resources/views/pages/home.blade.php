4a46f27439a60ea506e600b3ed9be60159fbbe72
Mark van Eijk <mark@vaneijk.co>
2020-01-24 11:29:14 +0100

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
 @section('main')
-    <h2>Hello</h2>
+    <h2>It works!</h2>
     <p>I'm a <a href="https://github.com/markvaneijk" rel="noopener">Web Developer</a>, <a href="https://vormkracht10.nl" rel="noopener" target="_blank">entrepeneur</a>, <a href="https://vaneijk.co" rel="noopener" target="_blank">company of one</a>, <a href="{{ route('posts') }}">tinkerer</a> and <a href="{{ route('now') }}">currently working</a> on <a href="https://rocketee.rs" rel="noopener" target="_blank">Rocketeers</a>.</p>
 @endsection
