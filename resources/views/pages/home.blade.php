11f3ad3689f2990c60e6aa84b2fbc242365c1540
Mark van Eijk <mark@vaneijk.co>
2022-08-16 23:49:24 +0200

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,10 +1,10 @@
 @extends('templates.master')
 
 @section('main')
-    <h2 class="mb-3 text-4xl font-bold leading-tight">Hallo 👋🏼,<br>I'm <x-link href="https://twitter.com/markvaneijk" rel="noopener" target="_blank">Mark.</x-link></h2>
+    <h2 class="mb-3 text-4xl font-bold leading-tight">Hallo 👋🏼,<br>I'm <x-link href="https://twitter.com/markvaneijk">Mark.</x-link></h2>
     <p class="text-2xl leading-relaxed">
-        I'm <x-link href="https://github.com/markvaneijk" rel="noopener" target="_blank">🧑🏻‍💻&nbsp;Full-Stack Maker of Webs</x-link>,
-        <x-link href="https://vormkracht10.nl" rel="noopener" target="_blank">entrepeneur</x-link>,
+        I'm <x-link href="https://github.com/markvaneijk">🧑🏻‍💻&nbsp;Full-Stack Maker of Webs,</x-link>
+        <x-link href="https://vormkracht10.nl">entrepeneur,</x-link>
         <x-link href="{{ route('post.index') }}">tinkerer</x-link> and
         <x-link href="{{ route('now') }}">currently working</x-link>
         on <x-link href="https://rocketee.rs">🚀&nbsp;Rocketeers</x-link> from <x-link href="https://www.nijmegen.nl">🇳🇱&nbsp;Nijmegen.</x-link>
