4ee4e4f68a64f886c1e68f521ab9d66e80330d14
Mark van Eijk <mark@vaneijk.co>
2022-08-18 11:01:46 +0200

    tech stack

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,14 +5,20 @@
 @section('main')
     <div class="md:py-20">
         <h2 class="mb-3 text-4xl font-bold leading-tight">Hallo 👋🏼,<br>I'm <x-link href="https://twitter.com/markvaneijk">Mark.</x-link></h2>
-        <p class="text-2xl leading-relaxed">
+        <p class="mb-6 text-2xl leading-relaxed">
             I'm <x-link href="https://github.com/markvaneijk">🧑🏻‍💻&nbsp;Full-Stack Maker of Webs,</x-link>
             <x-link href="https://vormkracht10.nl">entrepeneur,</x-link>
             <x-link href="{{ route('posts') }}">tinkerer</x-link> and
             <x-link href="{{ route('now') }}">currently working</x-link>
-            on <x-link href="https://rocketee.rs">🚀&nbsp;Rocketeers</x-link> from <x-link href="https://www.nijmegen.nl">🇳🇱&nbsp;Nijmegen.</x-link>
+            on <x-link href="https://rocketee.rs">🚀&nbsp;Rocketeers</x-link> and I live in <x-link href="https://www.nijmegen.nl">🇳🇱&nbsp;Nijmegen.</x-link>
         </p>
-        <div class="grid gap-4 mt-6 text-center md:grid-cols-3">
+        <p class="mb-6 text-2xl leading-relaxed">
+            I 💛 working with <x-link href="https://laravel.com" class="font-medium">#Laravel,</x-link>
+            <x-link href="https://reactjs.org" class="font-medium">#React,</x-link>
+            <x-link href="https://inertiajs.com" class="font-medium">#Inertia</x-link> and
+            <x-link href="https://tailwindcss.com" class="font-medium">#Tailwindcss.</x-link>
+        </p>
+        <div class="grid gap-4 text-center md:grid-cols-3">
             <x-button href="mailto:m@rkvaneijk.nl">E-mail</x-button>
             <x-button href="https://twitter.com/markvaneijk">Twitter</x-button>
             <x-button href="https://linkedin.com/in/markveijk">LinkedIn</x-button>
