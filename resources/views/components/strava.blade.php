7056a09c5b53d458127308611b4914c22ae5d12c
Mark van Eijk <mark@vaneijk.co>
2022-10-06 00:39:12 +0200

    wip

--- resources/views/components/strava.blade.php
+++ resources/views/components/strava.blade.php
@@ -1,4 +1,5 @@
-<a href="https://www.strava.com/athletes/9775556" target="_blank" class="hover:brightness-[1.15] bg-[#f83509] text-white px-12 py-6 mt-10 rounded shadow-lg transition hover:shadow-2xl ease-in-out duration-[1000ms] block">
+<a href="https://www.strava.com/athletes/9775556" target="_blank" class="hover:brightness-[1.15] bg-[#f83509] text-white p-12 mt-10 rounded shadow-lg transition hover:shadow-2xl ease-in-out duration-[1000ms] block">
+    <span class="text-[20px]">This month</span><br>
     <span class="text-[80px]">{{ $distance }}</span> <span class="text-[30px]">km</span><br>
     <span class="text-[80px]">{{ $prs }}</span>  <span class="text-[30px]">PRs</span>
 </a>
