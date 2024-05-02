e8c2f0e89f4c41dd101b6ee7eb20fcbce91c3efb
Mark van Eijk <mark@vaneijk.co>
2024-05-02 14:45:58 +0200

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,12 +6,12 @@
     <div class="md:py-20">
         <h2 class="mb-3 text-4xl font-bold leading-tight">Hallo 👋🏼,<br>I'm <x-link href="https://twitter.com/markvaneijk">Mark.</x-link></h2>
         <p class="mb-6 text-2xl leading-relaxed">
-            I'm a Laravel Developer,
+            I'm a Laravel Developer from <x-link href="https://www.nijmegen.nl">🇳🇱&nbsp;Nijmegen.</x-link>,
             <x-link href="https://github.com/markvaneijk">🧑🏻‍💻&nbsp;Full-Stack Maker of Webs,</x-link>
             <x-link href="https://vormkracht10.nl">entrepeneur,</x-link>
             <x-link href="{{ route('posts') }}">tinkerer</x-link> and
             <x-link href="{{ route('now') }}">currently working</x-link>
-            on <x-link href="https://rocketee.rs">🚀&nbsp;Rocketeers</x-link> and I live in <x-link href="https://www.nijmegen.nl">🇳🇱&nbsp;Nijmegen.</x-link>
+            on <x-link href="https://rocketee.rs">🚀&nbsp;Rocketeers</x-link>.
         </p>
         <p class="mb-6 text-2xl leading-relaxed">
             I 💛 working with <x-link href="https://laravel.com" class="font-medium">#Laravel,</x-link>
